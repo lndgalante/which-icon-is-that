d4515265620f51f760b4848e327b7b1ad27e98d6
leo <lndgalante@gmail.com>
2021-04-17 22:31:57 -0300

    📦 NEW: Add client-side analytics

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,33 +1,35 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
+import PlausibleProvider from 'next-plausible';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const meta = {
-  title: 'WhichIconIsThat',
-  description: 'Find your icon name and pack easily from your SVG',
   image: '/dps.png',
+  title: 'Which Icon Is That',
+  description: 'Find your icon name and pack easily from your SVG',
 };
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Head>
-        <meta name='robots' content='follow, index' />
-        <meta name='description' content={meta.description} />
-        <meta property='og:site_name' content={meta.title} />
-        <meta property='og:description' content={meta.description} />
-        <meta property='og:title' content={meta.title} />
-        <meta property='og:image' content={meta.image} />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@lndgalante' />
-        <meta name='twitter:title' content={meta.title} />
-        <meta name='twitter:description' content={meta.description} />
-        <meta name='twitter:image' content={meta.image} />
-        <title>Which Icon Is That</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <PlausibleProvider domain='whichiconisthat.com'>
+      <ChakraProvider>
+        <Head>
+          <meta name='robots' content='follow, index' />
+          <meta name='description' content={meta.description} />
+          <meta property='og:site_name' content={meta.title} />
+          <meta property='og:description' content={meta.description} />
+          <meta property='og:title' content={meta.title} />
+          <meta property='og:image' content={meta.image} />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:site' content='@lndgalante' />
+          <meta name='twitter:title' content={meta.title} />
+          <meta name='twitter:description' content={meta.description} />
+          <meta name='twitter:image' content={meta.image} />
+          <title>Which Icon Is That</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </PlausibleProvider>
   );
 }
