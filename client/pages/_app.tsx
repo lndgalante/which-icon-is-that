7bf095a3447eafb4d60b316da1d98f988095da5e
leo <lndgalante@gmail.com>
2021-04-17 21:51:28 -0300

    📦 NEW: Improve SEO adding meta tags

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,10 +2,27 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
+const meta = {
+  title: 'WhichIconIsThat',
+  description: 'Find your icon name and pack easily from your SVG',
+  image: '/dps.png',
+};
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Head>
+        <meta name='robots' content='follow, index' />
+        <meta name='description' content={meta.description} />
+        <meta property='og:site_name' content={meta.title} />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+        <meta property='og:image' content={meta.image} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@lndgalante' />
+        <meta name='twitter:title' content={meta.title} />
+        <meta name='twitter:description' content={meta.description} />
+        <meta name='twitter:image' content={meta.image} />
         <title>Which Icon Is That</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
