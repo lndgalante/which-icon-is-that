67a57cbdacdd5b44009a97fd515a2531fd20482f
leo <lndgalante@gmail.com>
2021-04-19 13:37:04 -0300

    📦 NEW: Revalidate every 1s

--- client/pages/[hash].tsx
+++ client/pages/[hash].tsx
@@ -250,7 +250,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { hash } = params;
   const data: FoundIcon = await api.getIconData(hash as string);
 
-  return { props: data, revalidate: false };
+  return { props: data, revalidate: 1 };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
