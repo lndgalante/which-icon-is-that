4382aadca08a7ac3670b0205150fff1567b4676c
leo <lndgalante@gmail.com>
2021-04-21 15:04:48 -0300

    📦 NEW: Add Figma link. Avoid creating map.

--- server/icons.ts
+++ server/icons.ts
@@ -1,11 +1,15 @@
-import { Md5 } from 'https://deno.land/std/hash/md5.ts';
+import { Redis } from 'https://deno.land/x/redis/mod.ts';
 import { readZip } from 'https://deno.land/x/jszip/mod.ts';
 import { sizeof } from 'https://deno.land/x/sizeof@v1.0.3/mod.ts';
 import { download, Destination } from 'https://deno.land/x/download/mod.ts';
 import { prettyBytes } from 'https://raw.githubusercontent.com/brunnerlivio/deno-pretty-bytes/master/mod.ts';
 
+// helpers
+import { createHash } from './hash.ts';
+import { getInnerHTMLFromSvgText } from './dom.ts';
+
 // types
-type PacksNames = keyof typeof ICONS_LINKS;
+type PacksNames = keyof typeof ICONS_WEBSITE_LINKS;
 
 export type Svg = {
   svg: string;
@@ -16,29 +20,34 @@ export type Svg = {
   iconFileName: string;
 };
 
-export type SvgMap = {
-  [key: string]: string;
-};
-
-// helpers
-const createHash = (value: string): string => new Md5().update(value.replace(/\s/g, '')).toString();
-
 // constants
-const ICONS_LINKS = {
+const GITHUB = 'https://github.com';
+
+const ICONS_WEBSITE_LINKS = {
+  heroicons: 'https://heroicons.com',
   feather: 'https://feathericons.com',
   bootstrap: 'https://icons.getbootstrap.com',
+};
+
+const ICONS_FIGMA_LINKS = {
+  heroicons: 'https://www.figma.com/community/file/958423903283802665',
+  bootstrap: 'https://www.figma.com/file/YjjMzXhECL1MIb6Qlm7VJO/Bootstrap-Icons-v1.4.1',
+  feather: 'https://www.figma.com/file/dyJRSFTIajik4cdkcXN8yA3K/Feather-Component-Library?node-id=0%3A1',
 };
 
 const ICONS_LIST = [
   { packId: 'bs', packName: 'bootstrap', owner: 'twbs', repo: 'icons' },
   { packId: 'fi', packName: 'feather', owner: 'feathericons', repo: 'feather' },
+  // { packId: 'hi', packName: 'heroicons', owner: 'tailwindlabs', repo: 'heroicons' },
 ];
 
 export function getIconSource(iconPack: PacksNames, iconFileName: string) {
   const links = {
-    feather: (iconFileName: string) => `https://github.com/feathericons/feather/blob/master/icons/${iconFileName}`,
-    bootstrap: (iconFileName: string) => `https://github.com/twbs/icons/blob/main/icons/${iconFileName}`,
+    bootstrap: (iconFileName: string) => `${GITHUB}/twbs/icons/blob/main/icons/${iconFileName}`,
+    heroicons: (iconFileName: string) => `${GITHUB}/twbs/icons/blob/main/icons/${iconFileName}`,
+    feather: (iconFileName: string) => `${GITHUB}/tailwindlabs/heroicons/blob/master/src/outline/${iconFileName}`,
   };
+
   const getIconSourceLinkFn = links[iconPack];
   const iconSourceLink = getIconSourceLinkFn(iconFileName);
 
@@ -47,8 +56,9 @@ export function getIconSource(iconPack: PacksNames, iconFileName: string) {
 
 export function getIconLink(iconPack: PacksNames, iconName: string) {
   const links = {
-    feather: (iconName: string) => `${ICONS_LINKS.feather}/?query=${iconName}`,
-    bootstrap: (iconName: string) => `${ICONS_LINKS.bootstrap}/icons/${iconName}`,
+    heroicons: () => ICONS_WEBSITE_LINKS.heroicons,
+    feather: (iconName: string) => `${ICONS_WEBSITE_LINKS.feather}/?query=${iconName}`,
+    bootstrap: (iconName: string) => `${ICONS_WEBSITE_LINKS.bootstrap}/icons/${iconName}`,
   };
   const getIconPackLinkFn = links[iconPack];
   const iconLink = getIconPackLinkFn(iconName);
@@ -57,13 +67,16 @@ export function getIconLink(iconPack: PacksNames, iconName: string) {
 }
 
 export function getIconPackWebsite(svgPackName: string) {
-  const iconPackWebsite = ICONS_LINKS[svgPackName as PacksNames];
+  const iconPackWebsite = ICONS_WEBSITE_LINKS[svgPackName as PacksNames];
   return iconPackWebsite;
 }
 
-export async function createSvgsMap() {
-  const result: SvgMap = {};
+export function getIconPackFigmaLink(svgPackName: string) {
+  const iconPackFigmaLink = ICONS_FIGMA_LINKS[svgPackName as PacksNames];
+  return iconPackFigmaLink;
+}
 
+export async function saveIconsIntoRedis(redis: Redis) {
   for (const { packId, packName, owner, repo } of ICONS_LIST) {
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/releases`);
     const [lastRelease] = await response.json();
@@ -89,14 +102,13 @@ export async function createSvgsMap() {
           const iconName = iconFileName.replace('.svg', '');
           const bytes = prettyBytes(sizeof(svg).bytesize);
 
-          const key = createHash(svg);
-          const value: Svg = { svg, bytes, packId, packName, iconName, iconFileName };
+          const svgInnerHtml = getInnerHTMLFromSvgText(svg);
+          const hash = createHash(svgInnerHtml);
+          const metadata: Svg = { svg, bytes, packId, packName, iconName, iconFileName };
 
-          result[key] = JSON.stringify(value);
+          await redis.set(hash, JSON.stringify(metadata));
         }
       }
     }
   }
-
-  return result;
 }
