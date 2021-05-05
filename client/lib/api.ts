6e59ded0cd5420933a177e9a8439a71f43129144
lndgalante <lndgalante@gmail.com>
2021-05-05 10:13:45 -0300

    📦 NEW: Use query params

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,10 +1,9 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const endpoints = {
-  icon: `${API_URL}/icon`,
+  icon: (hash) => `${API_URL}/icon/?hash=${hash}`,
 };
 
 export const api = {
-  getIconData: (hash: string) =>
-    fetch(endpoints.icon, { method: 'POST', body: JSON.stringify({ hash }) }).then((response) => response.json()),
+  getIconData: (hash: string) => fetch(endpoints.icon(hash)).then((response) => response.json()),
 };
