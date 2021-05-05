697c898c95b41a2384bcfc0a985c43935c8307da
lndgalante <lndgalante@gmail.com>
2021-05-05 10:12:57 -0300

    📦 NEW: Add Heroicons logo

--- client/components/icons/index.tsx
+++ client/components/icons/index.tsx
@@ -1,7 +1,9 @@
 import Feather from './Feather';
 import Bootstrap from './Bootstrap';
+import Heroicons from './Heroicons';
 
 export const ICONS_LOGOS = {
   feather: Feather,
   bootstrap: Bootstrap,
+  heroicons: Heroicons,
 };
