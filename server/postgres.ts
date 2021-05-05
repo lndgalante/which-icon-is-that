c91c04d94ab17f8091448257f133d7c6344e8986
lndgalante <lndgalante@gmail.com>
2021-05-05 10:25:55 -0300

    📦 NEW: Add connectToPostgres helper

---
+++ server/postgres.ts
@@ -0,0 +1,15 @@
+import 'https://deno.land/x/dotenv/load.ts';
+import { Client } from 'https://deno.land/x/postgres/mod.ts';
+
+export async function connectToPostgres() {
+  const client = new Client({
+    port: Deno.env.get('POSTGRES_PORT') as string,
+    user: Deno.env.get('POSTGRES_USER') as string,
+    password: Deno.env.get('POSTGRES_PASSWORD') as string,
+    hostname: Deno.env.get('POSTGRES_HOSTNAME') as string,
+    database: Deno.env.get('POSTGRES_DATABASE') as string,
+  });
+  await client.connect();
+
+  return client;
+}
