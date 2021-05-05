a768e4b92fc5949d2e870236c9400361e8ec3485
lndgalante <lndgalante@gmail.com>
2021-05-05 10:22:04 -0300

    🐛 FIX: Early return for empty svgs

--- server/dom.ts
+++ server/dom.ts
@@ -1,6 +1,8 @@
 import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
 
 export function getInnerHTMLFromSvgText(svg: string) {
-  const document = new DOMParser().parseFromString(svg, 'text/html')!;
-  return document.body.children[0].innerHTML;
+  if (!svg) return '';
+
+  const document = new DOMParser().parseFromString(svg, 'text/html');
+  return document ? document.body.children[0].innerHTML : '';
 }
