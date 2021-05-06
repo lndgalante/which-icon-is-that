ce383ba3f2705530d8b67655b5c5226c25cd2a9a
lndgalante <lndgalante@gmail.com>
2021-05-06 01:27:58 -0300

    📦 NEW: Add Antdesign and Boxicons logos

--- client/components/icons/index.tsx
+++ client/components/icons/index.tsx
@@ -1,9 +1,13 @@
 import Feather from './Feather';
 import Bootstrap from './Bootstrap';
 import Heroicons from './Heroicons';
+import Antdesign from './Antdesign';
+import Boxicons from './Boxicons';
 
 export const ICONS_LOGOS = {
   feather: Feather,
   bootstrap: Bootstrap,
   heroicons: Heroicons,
+  antdesign: Antdesign,
+  boxicons: Boxicons,
 };
