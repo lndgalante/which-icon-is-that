aac03e34903f46ab0e28a2b1c0a2a47dde3e0c60
lndgalante <lndgalante@gmail.com>
2021-05-07 08:43:18 -0300

    📦 NEW: Add new endpoints

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,9 +1,17 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+const get = (url: string) => fetch(url).then((response) => response.json());
+
 const endpoints = {
-  icon: (hash) => `${API_URL}/icon/?hash=${hash}`,
+  paths: () => `${API_URL}/paths`,
+  icon: (hash: string) => `${API_URL}/icon/?hash=${hash}`,
+  pathReverse: (path: string) => `${API_URL}/reverse/?path=${path}`,
+  hashReverse: (hash: string) => `${API_URL}/reverse/?hash=${hash}`,
 };
 
 export const api = {
-  getIconData: (hash: string) => fetch(endpoints.icon(hash)).then((response) => response.json()),
+  getPaths: () => get(endpoints.paths()),
+  getIconData: (hash: string) => get(endpoints.icon(hash)),
+  getHashFromPath: (path: string) => get(endpoints.pathReverse(path)),
+  getPathFromHash: (hash: string) => get(endpoints.hashReverse(hash)),
 };
