88c9553fafc72b64c3381cbd90545e4627a86263
lndgalante <lndgalante@gmail.com>
2021-05-09 22:06:27 -0300

    📦 NEW: Add getIconFoundData method

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,17 +1,25 @@
+// types
+type Methods = 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE';
+
+// constants
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-const get = (url: string) => fetch(url).then((response) => response.json());
+// helpers
+const fecher = (url: string, method: Methods = 'GET') => fetch(url, { method }).then((response) => response.json());
 
 const endpoints = {
   paths: () => `${API_URL}/paths`,
-  icon: (hash: string) => `${API_URL}/icon/?hash=${hash}`,
+  icon: (hash: string) => `${API_URL}/icon/${hash}`,
+  iconFound: (hash: string) => `${API_URL}/icon/${hash}/found`,
   pathReverse: (path: string) => `${API_URL}/reverse/?path=${path}`,
   hashReverse: (hash: string) => `${API_URL}/reverse/?hash=${hash}`,
 };
 
 export const api = {
-  getPaths: () => get(endpoints.paths()),
-  getIconData: (hash: string) => get(endpoints.icon(hash)),
-  getHashFromPath: (path: string) => get(endpoints.pathReverse(path)),
-  getPathFromHash: (hash: string) => get(endpoints.hashReverse(hash)),
+  getPaths: () => fecher(endpoints.paths()),
+  getIconData: (hash: string) => fecher(endpoints.icon(hash)),
+  getIconFoundData: (hash: string) => fecher(endpoints.iconFound(hash)),
+  getHashFromPath: (path: string) => fecher(endpoints.pathReverse(path)),
+  getPathFromHash: (hash: string) => fecher(endpoints.hashReverse(hash)),
+  putIconIncrement: (hash: string) => fecher(endpoints.icon(hash), 'PUT'),
 };
