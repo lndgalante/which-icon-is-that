693b44dae8e7dc852e8dca02238de5ebb58f0233
lndgalante <lndgalante@gmail.com>
2021-05-09 21:17:50 -0300

    👌 IMPROVE: getInnerHTMLFromSvgText helper

--- server/dom.ts
+++ server/dom.ts
@@ -1,6 +1,6 @@
 import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
 
 export function getInnerHTMLFromSvgText(svg: string): string {
-  const document = new DOMParser().parseFromString(svg, 'text/html');
-  return document?.querySelector('svg')?.innerHTML ?? '';
+  const document = new DOMParser().parseFromString(svg.toLowerCase(), 'text/html');
+  return document?.querySelector('svg')?.innerHTML?.toLowerCase() ?? '';
 }
