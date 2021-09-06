a1713a811ad81a35eb8a810345703890de744bdb
Leonardo Galante <lndgalante@gmail.com>
2021-09-06 02:02:26 -0300

    👌 IMPROVE: getInnerHTMLFromSvgText function

--- server/lib/dom.ts
+++ server/lib/dom.ts
@@ -4,7 +4,7 @@ export function getInnerHTMLFromSvgText(svg: string) {
   const document = new DOMParser().parseFromString(svg.toLowerCase(), 'text/html');
   const svgElement = document?.querySelector('svg');
 
-  const innerSvg = svgElement?.innerHTML?.toLowerCase() ?? '';
+  const innerSvg = svgElement?.innerHTML?.toLowerCase().trim() ?? '';
   const viewBox = svgElement?.getAttribute('viewBox') ?? '';
 
   return { innerSvg, viewBox };
