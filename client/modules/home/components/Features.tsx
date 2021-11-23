c9dc645f49beb5d1dd25c4a9d4c45d19b66782e5
Andrés Tonello <andresnicolastonello@gmail.com>
2021-11-23 17:06:48 -0300

    🐛 FIX: Features section layouts on desktop, tablet and mobile fixed

--- client/modules/home/components/Features.tsx
+++ client/modules/home/components/Features.tsx
@@ -17,11 +17,13 @@ export function Features() {
         Know our features
       </Text>
 
-      <Stack spacing={{ base: 10, md: 28 }}>
+      <Stack spacing={{ base: 10, md: 28 }} maxWidth="1440px">
         <Stack flexDirection={{ base: "column", md: "row" }} alignItems="center" as="article">
           <Stack
-            width={{ base: 335, md: 720 }}
-            height={{ base: 240, md: 900 }}
+            width={{ base: "100%", md: "50%" }}
+            maxWidth={{ base: 452, md: "100%" }}
+            py={{ base: "5%", md: "10%" }}
+            px={{ base: "5%", md: "0" }}
             backgroundColor="brand.softOrange"
             borderRadius={24}
             overflow="hidden"
@@ -29,29 +31,29 @@ export function Features() {
           >
             <Shapes.EasyTopRight
               position="absolute"
-              top={20}
-              right={85}
-              width={{ base: "2.0625rem", md: "6.4375rem" }}
+              width={{ base: "12%", md: "18%" }}
+              top={{ base: "50%", md: "8%" }}
+              right={{ base: "2%", md: "10%" }}
+              height="auto"
             />
             <Shapes.EasyBottomLeft
               position="absolute"
-              bottom={{ base: 28, md: 14 }}
-              left={{ base: 2, md: -2 }}
-              width={{ base: "3rem", md: "9.75rem" }}
-              transform={{ base: "rotate(260deg)", md: "none" }}
+              width={{ base: "10%", md: "20%" }}
+              bottom={{ base: "65%", md: "5%" }}
+              left={{ base: "2%", md: "0%" }}
+              transform={{ base: "rotate(230deg)", md: "none" }}
+              height="auto"
             />
             <Stack
-              width={{ base: 302, md: 876 }}
-              height={{ base: 215, md: 622 }}
-              top={{ base: 1, md: "8.2rem" }}
-              left={{ base: 4, md: 20 }}
-              position="absolute"
-              shadow="7"
-              borderRadius={{ base: 8, md: 16 }}
+              width={{ base: "100%", md: "140%" }}
+              pl={{ base: 0, md: "10%" }}
+              margin={{ base: "0 !important", md: 0 }}
+              pos="relative"
             >
-              <Image alt="Easy to find" src="/images/easy-to-find.png" />
+              <Image alt="Easy to find" src="/images/easy-to-find.png" shadow="7" borderRadius={{ base: 8, md: 16 }} />
             </Stack>
           </Stack>
+
           <Stack
             paddingLeft={{ base: 0, md: 20 }}
             paddingBottom={{ base: 8, md: 0 }}
@@ -81,7 +83,7 @@ export function Features() {
             Rich Icon Data
           </Text>
 
-          <Stack alignItems="center" spacing={{ base: 4, md: "3.65rem" }}>
+          <Stack alignItems="center" spacing={{ base: 4, md: "3.65rem" }} width="100%">
             <Text maxWidth={452} textAlign={{ base: "left", md: "center" }} fontSize={{ base: 14, md: 18 }}>
               All you need to know about your icon: Size, Weight,{" "}
               <Text as="span" fontWeight={700}>
@@ -94,31 +96,38 @@ export function Features() {
               backgroundColor="brand.softGrey"
               position="relative"
               borderRadius={24}
-              width={{ base: 335, md: 1064 }}
-              height={{ base: 240, md: 576 }}
+              width={{ base: "100%", md: "80%" }}
+              maxWidth={{ base: 452, md: 1064 }}
+              height={{ base: "auto", md: "40vw" }}
+              maxHeight={{ base: "auto", md: 576 }}
+              padding={{ base: "5%", md: "0" }}
+              alignItems="center"
               overflow="hidden"
             >
               <Shapes.RichTopRight
                 position="absolute"
-                top={{ base: 2, md: 40 }}
-                right={{ base: 4, md: 12 }}
-                width={{ base: "2.5rem", md: "6.625rem" }}
+                width={{ base: "13%", md: "10%" }}
+                top={{ base: "20%", md: "30%" }}
+                right={{ base: "2%", md: "5%" }}
+                height="auto"
               />
               <Shapes.RichBottomLeft
                 position="absolute"
-                bottom={{ base: -36, md: -28 }}
-                left={{ base: 2, md: 10 }}
-                width={{ base: "1.875rem", md: "5.875rem" }}
+                width={{ base: "10%", md: "12%" }}
+                bottom={{ base: "15%", md: "10%" }}
+                left={{ base: "2%", md: "5%" }}
+                height="auto"
               />
+
               <Stack
-                width={{ base: 302, md: 876 }}
-                height={{ base: 215, md: 622 }}
-                top={{ base: 1, md: 9 }}
-                left={{ base: 4, md: "5.8rem" }}
-                position="absolute"
+                width={{ base: "100%", md: "80%" }}
+                height={"auto"}
                 overflow="hidden"
                 borderRadius={{ base: 8, md: 16 }}
+                marginTop={{ md: "10% !important" }}
+                marginBottom={{ md: "-5% !important" }}
                 shadow="7"
+                zIndex={1}
               >
                 <Image alt="Rich Icon data" src="/images/rich-icon-data.png" />
               </Stack>
@@ -126,7 +135,7 @@ export function Features() {
           </Stack>
         </Stack>
 
-        <Stack flexDirection={{ base: "column", md: "row" }} alignItems="center" as="article">
+        <Stack flexDirection={{ base: "column", md: "row" }} alignItems="center" as="article" justifyContent="flex-end">
           <Stack
             paddingRight={{ base: 0, md: 20 }}
             paddingBottom={{ base: 8, md: 0 }}
@@ -146,8 +155,10 @@ export function Features() {
           </Stack>
 
           <Stack
-            width={{ base: 335, md: 720 }}
-            height={{ base: 240, md: 900 }}
+            width={{ base: "100%", md: "50%" }}
+            maxWidth={{ base: 452, md: "100%" }}
+            py={{ base: "5%", md: "10%" }}
+            px={{ base: "5%", md: "0" }}
             backgroundColor="brand.text"
             borderRadius={24}
             overflow="hidden"
@@ -155,26 +166,27 @@ export function Features() {
           >
             <Shapes.DeveloperBottomRight
               position="absolute"
-              bottom={{ base: -28, md: -10 }}
-              right={{ base: 1.5, md: "9.15rem" }}
-              width={{ base: "2.0625rem", md: "6.4375rem" }}
+              bottom={{ base: "15%", md: "6%" }}
+              right={{ base: 1.5, md: "20%" }}
+              width={{ base: "2.0625rem", md: "18%" }}
+              height="auto"
             />
             <Shapes.DeveloperTopLeft
               position="absolute"
-              top={{ base: 12, md: "5.4rem" }}
-              left={{ base: 10, md: "3.7rem" }}
-              width={{ base: "3rem", md: "9.75rem" }}
+              top={{ base: 12, md: "8%" }}
+              left={{ base: 10, md: "10%" }}
+              width={{ base: "3rem", md: "22%" }}
+              height="auto"
               transform={{ base: "rotate(180deg)", md: "none" }}
             />
 
             <Stack
-              width={{ base: 302, md: 876 }}
-              height={{ base: 215, md: 622 }}
-              top={{ base: 1, md: "8.2rem" }}
-              right={{ base: 4, md: 20 }}
-              position="absolute"
+              width={{ base: "100%", md: "120%" }}
+              height="auto"
               shadow="7"
               borderRadius={{ base: 8, md: 16 }}
+              marginLeft={{ base: 0, md: "-30% !important" }}
+              zIndex={1}
             >
               <Image alt="Developer panel" src="/images/developer-panel.png" />
             </Stack>
