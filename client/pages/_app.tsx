96a02a20b0e8795c7d6391eb068a92846008fefb
Leonardo Galante <lndgalante@gmail.com>
2021-11-29 19:12:15 -0300

    📦 NEW: Integrate Hotjar

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { NextSeo } from "next-seo";
 import { AppProps } from "next/app";
 import PlausibleProvider from "next-plausible";
-import { Stack, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 // components
 import { Fonts } from "@modules/common/components/Fonts";
-import { Navbar } from "@modules/common/components/Navbar";
-import { Footer } from "@modules/common/components/Footer";
+import { MainLayout } from "@modules/common/components/MainLayout";
 import { MotionFade } from "@modules/common/components/MotionFade";
+
+// hooks
+import { useHotjar } from "@modules/common/hooks/useHotjar";
 
 // theme
 import { theme } from "@modules/common/theme";
@@ -16,21 +18,9 @@ import { theme } from "@modules/common/theme";
 // constants
 const queryClient = new QueryClient();
 
-type LayoutProps = {
-  children: React.ReactNode;
-};
-
-function Layout({ children }: LayoutProps) {
-  return (
-    <Stack backgroundColor="brand.white" spacing={0}>
-      <Navbar />
-      {children}
-      <Footer />
-    </Stack>
-  );
-}
-
 export default function MyApp({ Component, pageProps, router }: AppProps) {
+  useHotjar();
+
   return (
     <PlausibleProvider domain="whichiconisthat.com">
       <QueryClientProvider client={queryClient}>
@@ -41,11 +31,11 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
             twitter={{ handle: "@whichiconisthat", site: "@whichiconisthat", cardType: "/images/dps.png" }}
           />
           <Fonts />
-          <Layout>
+          <MainLayout>
             <MotionFade as="main" key={router.route} withPadding>
               <Component {...pageProps} />
             </MotionFade>
-          </Layout>
+          </MainLayout>
         </ChakraProvider>
       </QueryClientProvider>
     </PlausibleProvider>
